36c2002126920124884148b1dd87702b4ea31d91
Cesar Augusto Ferreira <c3web@hotmail.com>
2019-09-26 23:54:31 -0300

    Adicionado Lombok

--- src/main/java/br/com/c3web/ceps/responses/CepResponse.java
+++ src/main/java/br/com/c3web/ceps/responses/CepResponse.java
@@ -2,44 +2,22 @@ package br.com.c3web.ceps.responses;
 
 import java.io.Serializable;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@EqualsAndHashCode @ToString
 public class CepResponse implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	@Getter @Setter
 	private String logradouro;
+	@Getter @Setter
 	private String bairro;
+	@Getter @Setter
 	private String cidade;
+	@Getter @Setter
 	private String estado;
-
-	public String getLogradouro() {
-		return logradouro;
-	}
-
-	public void setLogradouro(String logradouro) {
-		this.logradouro = logradouro;
-	}
-
-	public String getBairro() {
-		return bairro;
-	}
-
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
-
-	public String getCidade() {
-		return cidade;
-	}
-
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-
-	public String getEstado() {
-		return estado;
-	}
-
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
 
 }
